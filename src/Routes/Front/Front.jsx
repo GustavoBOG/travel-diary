0a4e1de3704f96fbc3369c527b0cccea0cd33e88
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2024-09-30 23:27:48 +0200

    cambio tamaños box front.css

--- src/Routes/Front/Front.jsx
+++ src/Routes/Front/Front.jsx
@@ -8,10 +8,10 @@ function Front() {
         <img src="/assets/image1.png" alt="salto angel " />
       </div>
       <div className="box">
-        <img src="/assets/image2.png" alt="salto angel " />
+        <img src="/assets/image2.png" alt="big " />
       </div>
       <div className="box">
-        <img src="/assets/image3.png" alt="salto angel " />
+        <img src="/assets/image3.png" alt="taj " />
       </div>
       <Carousel />
     </div>
