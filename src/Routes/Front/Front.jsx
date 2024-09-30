107b9c78fd3fbba96fcd080ac26a122b782067d3
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2024-09-30 21:48:59 +0200

    public assets

--- src/Routes/Front/Front.jsx
+++ src/Routes/Front/Front.jsx
@@ -5,13 +5,13 @@ function Front() {
   return (
     <div className="front-container">
       <div className="box">
-        <img src="../../src/assets/image1.png" alt="salto angel " />
+        <img src="/assets/image1.png" alt="salto angel " />
       </div>
       <div className="box">
-      <img src="../../src/assets/image2.png" alt="salto angel " />
+        <img src="/assets/image2.png" alt="salto angel " />
       </div>
       <div className="box">
-      <img src="../../src/assets/image3.png" alt="salto angel " />
+        <img src="/assets/image3.png" alt="salto angel " />
       </div>
       <Carousel />
     </div>
